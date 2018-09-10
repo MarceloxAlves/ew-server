d4d725d2723979d8ffcc5ffec73634f844d831cd
Marcelo <marceloalvessoft@gmail.com>
2018-09-10 16:38:57 -0300

    Test URL

--- app.js
+++ app.js
@@ -29,14 +29,12 @@ var corsOptions = {
   maxAge: 3600
 }
 
-app.use(cors());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+//   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 function collectioner(arr, res, err){
 
@@ -44,6 +42,10 @@ function collectioner(arr, res, err){
 
 app.get('/', function(req, res){
   res.send('server is running');
+});
+
+app.get('/teste', function(req, res){
+    res.json('server is running');
 });
 
 app.get('/professor', function(req, res, next) {
