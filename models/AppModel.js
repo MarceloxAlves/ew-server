0947bc6fa06183845d07e946903b3cb8202c8b9d
Marcelo <marceloalvessoft@gmail.com>
2018-09-10 21:19:02 -0300

    URL DATABASE

--- models/AppModel.js
+++ models/AppModel.js
@@ -1,11 +1,7 @@
 const  mysql = require('mysql-model');
 module.exports  = {
   construct: function(table) {
-   let appmodel =  mysql.createConnection({
-     host: "31.170.161.231",
-     user: "itranspa_marcelo",
-     password: "Marcelo10",
-     database: "itranspa_editworld"});
+   let appmodel =  mysql.createConnection(process.env.DATABASE.URL);
     return  appmodel.extend({
       tableName: table,
     });
