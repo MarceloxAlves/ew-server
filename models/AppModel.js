d60c3f6ff95ee8849f3f195e3c01d9d66905d273
Marcelo <marceloalvessoft@gmail.com>
2018-09-10 21:23:25 -0300

    URL DATABASE

--- models/AppModel.js
+++ models/AppModel.js
@@ -1,7 +1,7 @@
 const  mysql = require('mysql-model');
 module.exports  = {
   construct: function(table) {
-   let appmodel =  mysql.createConnection(process.env.DATABASE.URL);
+   let appmodel =  mysql.createConnection(process.env.DATABASE_URL);
     return  appmodel.extend({
       tableName: table,
     });
