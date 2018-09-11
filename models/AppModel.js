35d6a78b8cf8006bf07689c923997b36ce8af157
Marcelo <marceloalvessoft@gmail.com>
2018-09-10 21:57:02 -0300

    Test URL

--- models/AppModel.js
+++ models/AppModel.js
@@ -1,7 +1,14 @@
 const  mysql = require('mysql-model');
 module.exports  = {
   construct: function(table) {
-   let appmodel =  mysql.createConnection(process.env.DATABASE_URL);
+   let appmodel =  mysql.createConnection(
+       process.env.DATABASE_URI || {
+           host: 'localhost',
+           user: 'root',
+           database: 'editworld',
+           password: ''
+       }
+   );
     return  appmodel.extend({
       tableName: table,
     });
