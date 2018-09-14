ae719f52e31d1d8616bc111244cdc2b108a66b62
Marcelo Alves <marceloalvessoft@gmail.com>
2018-09-14 09:01:51 -0300

    application/json

--- index.js
+++ index.js
@@ -66,11 +66,8 @@ app.post('/professor/salvar', function(req, res, next) {
 });
 
 app.post('/professor/login', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
- Professor.construct(res, req.body.email, req.body.senha)
+    res.writeHead(200,{"Content-Type":"application/json"});
+    Professor.construct(res, req.body.email, req.body.senha)
 });
 
 
